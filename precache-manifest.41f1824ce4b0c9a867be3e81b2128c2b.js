323c5739b09dbb7af4907a37184ffcdc37b7f4d0
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2019-10-06 13:41:51 +0200

    Updates

---
+++ precache-manifest.41f1824ce4b0c9a867be3e81b2128c2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ca1d6ccadbdb8f39f73bd687295117",
+    "url": "/jorgedelafuente/pokeapp/index.html"
+  },
+  {
+    "revision": "8d8ba2b54511d9ede1e8",
+    "url": "/jorgedelafuente/pokeapp/static/css/main.362d8fa3.chunk.css"
+  },
+  {
+    "revision": "d2b0c0b71005fe2a6aac",
+    "url": "/jorgedelafuente/pokeapp/static/js/2.f77af13e.chunk.js"
+  },
+  {
+    "revision": "8d8ba2b54511d9ede1e8",
+    "url": "/jorgedelafuente/pokeapp/static/js/main.53e40e9e.chunk.js"
+  },
+  {
+    "revision": "013eeeb6afdb138a5249",
+    "url": "/jorgedelafuente/pokeapp/static/js/runtime-main.e4cf986a.js"
+  }
+]);
